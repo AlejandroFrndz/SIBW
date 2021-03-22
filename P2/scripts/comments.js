46c4d8516c5ab70426186fb581b53ba4fb3733fd
AlejandroFrndz <fernandezalcaidealejandro@gmail.com>
2021-03-22 12:35:53 +0100

    Fixed double button for comments Now theres only one button which switches text accordingly

--- P2/scripts/comments.js
+++ P2/scripts/comments.js
@@ -1,7 +1,6 @@
 function toggleComments() {
   var com = document.getElementById("commentSection");
   var show = document.getElementById("showButton");
-  var hide = document.getElementById("hideButton");
 
   if(com.style.display === ""){
     com.style.display = "none";
@@ -9,12 +8,10 @@ function toggleComments() {
 
   if (com.style.display === "none") {
     com.style.display = "block";
-    show.style.display = "none";
-    hide.style.display = "block";
+    show.innerText = "Hide Comments";
   } else {
     com.style.display = "none";
-    show.style.display = "block";
-    hide.style.display = "none";
+    show.innerText = "Show Comments";
   }
 }
 
